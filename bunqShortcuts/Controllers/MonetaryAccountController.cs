d01e06727c7a14e141b27f27b227e0c65a0aeed0
WouterJanson <wouterjanson@icloud.com>
2019-06-10 11:46:28 +0200

    Initial commit

--- bunqShortcuts/Controllers/MonetaryAccountController.cs
+++ bunqShortcuts/Controllers/MonetaryAccountController.cs
@@ -1,10 +1,64 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using Bunq.Sdk.Context;
+using Bunq.Sdk.Model.Generated.Endpoint;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
 namespace bunqShortcuts.Controllers
 {
-    public class MonetaryAccountController
+    public class Account
     {
-        public MonetaryAccountController()
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Balance { get; set; }
+    }
+
+    [Route("api/[controller]")]
+    public class MonetaryAccountController : Controller
+    {
+        // GET: api/MonetaryAccount
+        [HttpGet]
+        [Authorize]
+        public JsonResult Get()
         {
+            var apiContext = ApiContext.Restore();
+            BunqContext.LoadApiContext(apiContext);
+
+            var monetaryAccounts = MonetaryAccount.List();
+            var accounts = new List<Account>();
+            foreach (var acc in monetaryAccounts.Value) {
+                if (acc.MonetaryAccountBank != null) {
+                    var account = new Account {
+                        Id = acc.MonetaryAccountBank.Id.ToString(),
+                        Name = acc.MonetaryAccountBank.Description,
+                        Balance = $"€{acc.MonetaryAccountBank.Balance.Value}"
+                    };
+                    accounts.Add(account);
+                }
+            }
+            return Json(accounts);
+        }
+
+        // GET api/monetaryaccount/ID
+        [HttpGet("{id}")]
+        [Authorize]
+        public ActionResult Get(int id)
+        {
+            var apiContext = ApiContext.Restore();
+            BunqContext.LoadApiContext(apiContext);
+
+            var monetaryAccount = MonetaryAccount.Get(id);
+            if (monetaryAccount.Value.MonetaryAccountBank == null) {
+                return BadRequest();
+            }
+
+            var account = new Account {
+                Id = monetaryAccount.Value.MonetaryAccountBank.Id.ToString(),
+                Name = monetaryAccount.Value.MonetaryAccountBank.Description,
+                Balance = $"€{monetaryAccount.Value.MonetaryAccountBank.Balance.Value}"
+            };
+
+            return Json(account);
         }
     }
 }
--- bunqShortcuts/Startup.cs
+++ bunqShortcuts/Startup.cs
@@ -1,23 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Security.Claims;
 using System.Threading.Tasks;
+using AspNet.Security.ApiKey.Providers;
+using AspNet.Security.ApiKey.Providers.Events;
+using AspNet.Security.ApiKey.Providers.Extensions;
+using Bunq.Sdk.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace bunqShortcuts
 {
     public class Startup
     {
+        private const string API_KEY = "<< Your bunq API Key >>";
+        private const string DEVICE_DESCRIPTION = "bunqShortcuts";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
+            // Init bunq API
+            try {
+                var apiContext = ApiContext.Restore();
+                apiContext.Save();
+            } catch (Bunq.Sdk.Exception.BunqException) {
+                var apiContext = ApiContext.Create(ApiEnvironmentType.PRODUCTION, API_KEY, DEVICE_DESCRIPTION);
+                apiContext.Save();
+            }
         }
 
         public IConfiguration Configuration { get; }
@@ -26,6 +37,31 @@ namespace bunqShortcuts
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddAuthentication(options => {
+                options.DefaultAuthenticateScheme = ApiKeyDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = ApiKeyDefaults.AuthenticationScheme;
+            })
+                .AddApiKey(options => {
+                    options.Header = "Authorization";
+                    options.HeaderKey = "ApiKey";
+                    options.Events = new ApiKeyEvents {
+                        OnAuthenticationFailed = context => {
+                            context.Fail(context.Exception);
+                            return Task.CompletedTask;
+                        },
+                        OnApiKeyValidated = context => {
+                            if (context.ApiKey == "Super-secret-key") {
+                                var identity = new ClaimsIdentity(new[] {
+                                    new Claim(ClaimTypes.Name, "W. Janson")
+                                });
+                                context.Principal.AddIdentity(identity);
+                                context.Success();
+                            }
+                            return Task.CompletedTask;
+                        }
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +78,7 @@ namespace bunqShortcuts
             }
 
             app.UseHttpsRedirection();
+            app.UseAuthentication();
             app.UseMvc();
         }
     }
